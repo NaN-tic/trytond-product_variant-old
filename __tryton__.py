ecc40cc0b4fa26e281e857449eac10149bc64e0b
resteve <resteve@zikzakmedia.com>
2012-11-14 17:24:37 +0100

    Header info module

--- __tryton__.py
+++ __tryton__.py
@@ -1,12 +1,13 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
+#This file is part product_variant module for Tryton.
+#The COPYRIGHT file at the top level of this repository contains
+#the full copyright notices and license terms.
 {
     'name': 'Product Variants',
     'version': '2.3.0',
     'author': 'grasbauer',
     'email': 'info@grasbauer.com',
     'website': 'http://www.grasbauer.com/',
-    'description': """Manage variants of products with automatic 
+    'description': """Manage variants of products with automatic
 generation based on attributes.""",
     'depends': [
         'ir',
