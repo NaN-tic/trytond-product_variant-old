e57892e75d66d0cec9c6ae5c15a95293c0ca7eee
resteve <resteve@zikzakmedia.com>
2015-12-01 11:16:11 +0100

    Tests

--- tests/test_product_variant.py
+++ tests/test_product_variant.py
@@ -1,34 +1,18 @@
-#!/usr/bin/env python
-# This file is part product_variant module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the product_variant module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
-import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
-from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class ProductVariantTestCase(unittest.TestCase):
+class ProductVariantTestCase(ModuleTestCase):
     'Test Product Variant module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('product_variant')
-
-    def test0005views(self):
-        'Test views'
-        test_view('product_variant')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'product_variant'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         ProductVariantTestCase))
-    suite.addTests(doctest.DocFileSuite('scenario_product_variant.rst',
-            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
-            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
